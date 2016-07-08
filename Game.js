520998a505ee964ff5e18884f50c1d3761f510c8
Mikko Heikelä <mikko.heikela@futurice.com>
2016-07-08 22:26:29 +0100

    Signup stage for games

--- Game.js
+++ Game.js
@@ -1,11 +1,17 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-export const game = (oldState = {}, action) => {
+const initialState = {
+  stage: 'EMPTY',
+  activeGameRules: null
+};
+
+export const game = (oldState = initialState, action) => {
   switch (action.type) {
     case 'START_GAME':
       return {
         ...oldState,
+        stage: 'SIGNUP',
         activeGameRules: action.rules
       };
     default: return oldState;
@@ -13,27 +19,29 @@ export const game = (oldState = {}, action) => {
 };
 
 const GamePresentational = (props) => {
-  if (props.rules) {
-    return (
-      <div>
-        Playing a game of {props.rules.title}
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        No game selected
-      </div>
-    );
+  switch (props.stage) {
+    case 'EMPTY': return (
+        <div>
+          No game selected
+        </div>
+      );
+    case 'SIGNUP': return (
+        <div>
+          Signing up players for a game of {props.rules.title}
+        </div>
+      );
+    default: return <div>Unknown game state</div>;
   }
 };
 GamePresentational.propTypes = {
+  stage: PropTypes.string.isRequired,
   rules: PropTypes.shape({
     title: PropTypes.string.isRequired
   })
 };
 const mapStateToProps = (state) => (
   {
+    stage: state.game.stage,
     rules: state.game.activeGameRules
   }
 );
