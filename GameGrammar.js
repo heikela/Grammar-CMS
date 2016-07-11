df8ae509851afb591409c9a4c43be68f4d7a3077
Mikko Heikelä <mikko.heikela@futurice.com>
2016-07-11 13:38:43 +0100

    Add the possibility for games to have a grid as their components

--- GameGrammar.js
+++ GameGrammar.js
@@ -1,10 +1,17 @@
 import {
   Grammar,
-  SequenceExpansion
+  SequenceExpansion,
+  RepeatExpansion,
+  AlternativesExpansion
 } from './grammar';
 
 export const gameGrammar = new Grammar(
   {
-    root: new SequenceExpansion(['title', 'players'])
+    root: new SequenceExpansion(['title', 'players', 'components']),
+    components: new RepeatExpansion('component'),
+    component: new AlternativesExpansion([
+        'grid'
+    ]),
+    grid: new SequenceExpansion(['title', 'width', 'height'])
   }
 );
